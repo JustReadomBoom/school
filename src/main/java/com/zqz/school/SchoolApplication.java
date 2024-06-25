f47d260fd7294f44198e2ca2b94d4268fd82ee05
zqz <962370325@qq.com>
2024-06-25 14:21:15 +0800

    123

---
+++ src/main/java/com/zqz/school/SchoolApplication.java
@@ -0,0 +1,19 @@
+package com.zqz.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zqz
+ * @CreateTime: 2024/06/14
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zqz/school/mapper/UserRoleMapper.java
@@ -0,0 +1,10 @@
+package com.zqz.school.mapper;
+
+/**
+ * @Author: zqz
+ * @CreateTime: 2024/06/14
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public interface UserRoleMapper {
+}
